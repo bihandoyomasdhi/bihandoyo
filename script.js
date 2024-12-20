fcd1ca759456255018d292c6b34bb15ff9630dc3
bihandoyomasdhi <64964616+bihandoyomasdhi@users.noreply.github.com>
2024-12-20 14:46:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Inisialisasi Swiper untuk setiap item portfolio
+document.addEventListener("DOMContentLoaded", function() {
+    var swipers = document.querySelectorAll('.swiper-container');
+    swipers.forEach(function(swiperElement) {
+        new Swiper(swiperElement, {
+            loop: true,
+            navigation: {
+                nextEl: swiperElement.querySelector('.swiper-button-next'),
+                prevEl: swiperElement.querySelector('.swiper-button-prev'),
+            },
+    
+                delay: 5000, // Ganti dengan delay sesuai kebutuhan
+        
+        });
+    });
+});
+
+
+
+
